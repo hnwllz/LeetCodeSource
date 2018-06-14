bc77948b612f19220d5bac4c7f42bc16fdd0dda6
longzhi <hnwllz@163.com>
2018-06-14 17:16:06 +0800

    提交 另外两种可以通过的方式

--- ReverseString/csharp/Solution.cs
+++ ReverseString/csharp/Solution.cs
@@ -12,5 +12,28 @@ public  string ReverseString(string s) {
     
         return new string(a);
     }
+
+//方法二，同方法一类似，不同的是直接用数组反向存储字符串的字符即可
+public  string ReverseString1(string s) {
+        if(s == null || s.Length < 2) return s;
+    
+        char[] array = new char[s.Length];
+        for(int i= s.Length - 1;i> -1;i--){
+            array[s.Length - i - 1] = s[i]; 
+        }
+    
+        return new string(array);
+    }
    
 };
+//方法三、同方法二，用c#中已有的StringBuilder代替字符数组来构建字符串。这种方法比方法二简洁高效。
+public  string ReverseString(string s) {
+        if(s == null || s.Length < 2) return s;
+    
+        StringBuilder appender = new StringBuilder();
+        for(int i= s.Length - 1;i> -1;i--){
+            appender.Append(s[i]); 
+        }
+    
+        return appender.ToString();
+    }
